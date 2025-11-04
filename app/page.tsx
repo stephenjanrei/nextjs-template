ea4c7972f346300961be056fb45eb7884f7d4749
Stephen Janrei <stephen@vidaura.com>
2025-11-04 16:12:18 +0800

    chore: setup nextjs 16 and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-dvh flex flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+    </main>
+  )
+}
